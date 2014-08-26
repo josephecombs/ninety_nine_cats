dd07ddb59f0172dd189dde974964b764c29aad09
Joseph Combs <joseph.e.combs@gmail.com>
2014-08-26 10:16:58 -0700

    more cat stuff

--- app/controllers/cats_controller_controller.rb
+++ app/controllers/cats_controller_controller.rb
@@ -1,2 +1,16 @@
 class CatsControllerController < ApplicationController
+  
+  def index
+    render :index
+  end
+  
+  def show
+    render :show
+  end
+  
+  private
+  def cat_params
+    cat_attrs = [:name, :age, :sex, :birth_date, :description, :color]
+    params.require(:cat).permit(*cat_attrs)
+  end
 end
